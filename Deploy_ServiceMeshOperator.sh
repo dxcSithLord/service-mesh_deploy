a526ae1c40a4efa350bcb8a2f014afc79b63fea0
Alistair Amabile <48163392+dxcSithLord@users.noreply.github.com>
2024-07-24 22:45:42 +0100

    resolve script errors

--- Deploy_ServiceMeshOperator.sh
+++ Deploy_ServiceMeshOperator.sh
@@ -20,16 +20,15 @@
 
 Test_object_exists() {
   if (( $# == 2 )); then
-    local obj_type=="${1}"
-    local obj_name=="${2}"
-    x=$(oc get ${obj_type} ${obj_name} -o template --template '{{.status.phase}}/{{.metadata.creationTimestamp}}' 2>/dev/null)
-    if (( $? == 0 )); then
-      echo "Object name '${obj_name}' of object type '${obj_type}' already exists - state/Created=${x}"
-      return 255
-    else
-      echo "Object name '${obj_name}' of object type '${obj_type}'does not exist in '$(oc project -q)'"
+    local obj_type="${1}"
+    local obj_name="${2}"
+    x=$(oc get "${obj_type}" "${obj_name}" -o template \
+        --template '{{.status.phase}}/{{.metadata.creationTimestamp}}' 2>/dev/null)
+    (( $? == 0 )) && \
+      echo "Object name '${obj_name}' of object type '${obj_type}' already exists - state/Created=${x}" && \
+      return 255 || \
+      echo "Object name '${obj_name}' of object type '${obj_type}'does not exist in '$(oc project -q)'" && \
       return 0
-    fi
   else
     echo "Incorrect number of arguments, object_type and object_name expected"
     return 99
@@ -38,52 +37,45 @@ Test_object_exists() {
 
 Load_Operator_Deps() {
   if (( $# == 2 )); then
-    local operator_name=="${1}"
-    local op_ns=="${2}"
-  
+    local operator_name="${1}"
+    local op_ns="${2}"
+    set -a task_seq
+    task_seq=( "service-account" "OperatorGroup" "Subscription" )
+    declare -rA obj_types=( ["service-account"]="apply" \
+                            ["OperatorGroup"]="create" \
+                            ["Subscription"]="apply" )
     # test for the if does not exists, then create
-    y=$(Test_object_exists namespace ${op_ns})
-    case $? in 
+
+    case $(Test_object_exists namespace "${op_ns}") in
         0 )   
-          echo "Creating new objects" ;;
-          oc create ns ${op_ns}
-          if (( $? == 0 )); then
-            oc apply -f "${operator_name}_service-account.yaml"
-            if (( $? == 0 )); then
-              oc create -f "${operator_name}_OperatorGroup.yaml"
-              if (( $? == 0 )); then
-                oc apply -f "${operator_name}_Subscription.yaml"
-                if (( $? > 0 )); then
-                  echo "WARNING: Applying ${operator_name} Subscription had a problem, please check"
-                  exit 1
-                fi
+          echo "Creating new objects"
+          if oc create ns "${op_ns}"; then
+            for obj in "${task_seq[@]}"; do
+              if oc "${obj_types[obj]}" -f "${operator_name}_${obj}.yaml";  then
+                echo "oc ${obj_types[obj]} -f ${operator_name}_${obj}.yaml OK"
               else
-                echo "WARNING: Creating ${operator_name} OperatorGroup had a problem, please check"
+                echo "WARNING: Applying ${operator_name} ${obj} had a problem, please check"
                 exit 1
               fi
-            else
-              echo "WARNING: Applying ${operator_name} service-account had a problem, please check"
-              exit 1
-            fi
+            done
           else
-            echo "WARNING: Creating ${operator_name} had a problem, please check"
-            exit 1
-          fi
+            echo "WARNING: Creating namespace ${op_ns} had problem"
+          fi;;
       255 )   
-        echo "already there" ;;
-          # switch to the openshift-operators-redhat project namespace
-          oc project ${op_ns}
+        echo "already there"
+          # Hence switch to the openshift-operators-redhat project namespace
+          #  to perform checks
+          oc project "${op_ns}"
           #  work out the values to pass from the yaml file - needs yq installed.
-          y=$(Test_object_exists ServiceAccount XXXXX_FROM_YAML_XXXX )
-          if (( $? == 0 )); then
-            oc apply -f "${operator_name}_service-account.yaml"
-            if (( $? == 0 )); then
+          sa_name=$(yq '.metadata.name' "${operator_name}_service-account.yaml")
+          if Test_object_exists ServiceAccount "${sa_name}"; then
+            # True is does NOT exist
+            if oc apply -f "${operator_name}_service-account.yaml"; then
               # get the number of existing OperatorGroups
-              opgroups=$(oc get OperatorGroup --no-headers -o json )
-              if (( $? == 0 && $(echo $opgroups | jq '.items | length') == 0 )); then
+              op_groups=$(oc get OperatorGroup --no-headers -o json )
+              if (( $? == 0 && $(echo "$op_groups" | jq '.items | length' ) == 0 )); then
                 # if none found and OK to create
-                oc create -f "${operator_name}_OperatorGroup.yaml"
-                if (( $? == 0 )); then
+                if oc create -f "${operator_name}_OperatorGroup.yaml"; then
                   oc apply -f "${operator_name}_Subscription.yaml"
                 else
                   echo "WARNING: ${operator_name} Subscription had a problem, please check"
@@ -92,21 +84,24 @@ Load_Operator_Deps() {
               else
                 # Operator groups exist, so list them and exit
                 echo "Operator groups exist - please verify openshift-operators-redhat-* exists"
-                echo "$opgroups" | jq '.items'
+                echo "$op_groups" | jq '.items'
+              fi
             else
               echo "WARNING: ${operator_name} OperatorGroup had a problem, please check"
               exit 1
             fi
-    
+          else
+            echo "Well that didn't work"
+          fi ;;
        99 )   
-        echo "wrong args" ;;
-          exit 1
+        echo "wrong args"
+          exit 1 ;;
     esac
   else
     echo "Incorrect number of arguments, operator_name and namespace expected"
     return 99
   fi
-}  
+}  # end of Load_Operator_Deps
  
 # else
   
